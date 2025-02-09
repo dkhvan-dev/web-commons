ccdda12b30b5a2f2d55a2fdf25c159d3acdc22b0
dkhvan <dkhvan.developer@gmail.com>
2025-02-10 00:31:16 +0700

    refactor: Add load standard localization error msgs

--- config/localization.go
+++ config/localization.go
@@ -19,6 +19,18 @@ func InitLocalization(paths []string) error {
 		Bundle = i18n.NewBundle(language.English)
 		Bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
 
+		standardPaths := []string{
+			"./errors/msgs/errors.en.json",
+			"./errors/msgs/errors.ru.json",
+			"./errors/msgs/errors.kk.json",
+		}
+		for _, path := range standardPaths {
+			if _, loadErr := Bundle.LoadMessageFile(path); loadErr != nil {
+				err = fmt.Errorf("failed to load standard error messages: %w", err)
+				return
+			}
+		}
+
 		for _, path := range paths {
 			if _, loadErr := Bundle.LoadMessageFile(path); loadErr != nil {
 				err = fmt.Errorf("failed to load translations '%s': %w", path, loadErr)
