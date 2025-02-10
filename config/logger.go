d4b111ed8d9943e8cea8bee43b78ddc102a43c6d
dkhvan <dkhvan.developer@gmail.com>
2025-02-10 14:43:43 +0700

    feat: Add logger

---
+++ config/logger.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var Logger *zap.Logger
+
+func InitLogger() {
+	logger := zap.New(zapcore.NewCore(zapcore.NewJSONEncoder(
+		zapcore.EncoderConfig{
+			TimeKey:        "@timestamp",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.RFC3339NanoTimeEncoder,
+			EncodeDuration: zapcore.NanosDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}),
+		zapcore.AddSync(os.Stdout),
+		zap.NewAtomicLevelAt(zap.InfoLevel),
+	), zap.AddCaller(), zap.AddCallerSkip(1))
+
+	defer logger.Sync()
+	Logger = logger
+}
